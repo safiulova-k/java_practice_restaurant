6645493681adef3711b6ba6bacf00910eb106aa3
kamilia <Admin@DESKTOP-NFSG25N>
2025-11-17 00:40:40 +0400

    чуть-чуть исправила...

--- src/main/java/com/safiulova/restaurant_rating/entity/Restaurant.java
+++ src/main/java/com/safiulova/restaurant_rating/entity/Restaurant.java
@@ -3,6 +3,9 @@ package com.safiulova.restaurant_rating.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.DecimalMin;
 
 import java.math.BigDecimal;
 
@@ -10,10 +13,23 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Restaurant {
+
+    @NotNull
     private Long id;
+
+    @NotNull
     private String name;
+
+    @Nullable
     private String description;
+
+    @NotNull
     private CuisineType cuisineType;
+
+    @NotNull
+    @DecimalMin("0.0")
     private BigDecimal averageCheck;
+
+    @NotNull
     private BigDecimal rating;
 }
--- src/main/java/com/safiulova/restaurant_rating/entity/Review.java
+++ src/main/java/com/safiulova/restaurant_rating/entity/Review.java
@@ -1,15 +1,29 @@
 package com.safiulova.restaurant_rating.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
+
+    @NotNull
     private Long visitorId;
+
+    @NotNull
     private Long restaurantId;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
     private int score;
+
+    @Nullable
     private String text;
 }
