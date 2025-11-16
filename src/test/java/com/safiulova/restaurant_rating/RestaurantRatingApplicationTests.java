1f154c0f235f057187d8523bfb0258d815828490
kamilia <Admin@DESKTOP-NFSG25N>
2025-11-16 21:14:49 +0400

    init

---
+++ src/test/java/com/safiulova/restaurant_rating/RestaurantRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safiulova.restaurant_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
